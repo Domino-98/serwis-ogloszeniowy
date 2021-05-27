5decd9122036a7fee4c4381a726524a5f513d6c2
Dominik Buczek <bdominoo@gmail.com>
2021-05-27 14:08:41 +0200

    Create session and add flash

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 
 const ads = require('./routes/ads');
@@ -24,23 +26,43 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-app.use(favicon(path.join(__dirname,'public','images','logo.ico')));
-app.use('/public', express.static('public')); 
-
+mongoose.set('useFindAndModify', false);
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(favicon(path.join(__dirname,'public','images','logo.ico')));
+app.use('/public', express.static('public'));
 
+const sessionConfig = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:  1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+ 
 app.use('/ads', ads);
 app.use('/category', category);
 
 app.get('/', (req, res) => {
     res.render('home');
 });
+
 
 
 app.all('*', (req, res, next) => {
