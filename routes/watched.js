1cd33c9994a47935e3dcfc202150c2e093c2ff75
Dominik Buczek <bdominoo@gmail.com>
2021-05-31 21:10:10 +0200

    Fix watch category redirect

--- routes/watched.js
+++ routes/watched.js
@@ -6,7 +6,6 @@ const User = require('../models/user');
 
 let currentPage = require('../routes/ads');
 
-
 router.get('/', async(req, res) => {
     if (!req.user) {
         req.flash('error', 'Musisz być zalogowany');
@@ -21,28 +20,33 @@ router.get('/', async(req, res) => {
 router.get('/:id/add', async(req, res) => {
     const ad = await Ad.findOne({_id: req.params.id});
     if (!req.user) {
-        console.log(currentPage.editUrl);
         req.flash('error', 'Musisz być zalogowany aby móc dodać ogłoszenie do obserwowanych');
         checkUrlAndRedirect();
     }
 
-    const loggedUser = await User.findOne({_id: req.user._id});
-    const adToAdd = await User.findOne({_id: req.user._id, 'watchedAds': ad._id});
-
-    if (adToAdd == null) {    
-        loggedUser.watchedAds.push(ad);
-        await loggedUser.save();
-        req.flash('success', 'Dodano ogłoszenie do obserwowanych');
-        checkUrlAndRedirect();
-    } else {
-        req.flash('error', 'Te ogłoszenie jest już w twoich obserwowanych');
-        checkUrlAndRedirect();
+    if (req.user) {
+        const loggedUser = await User.findOne({_id: req.user._id});
+        const adToAdd = await User.findOne({_id: req.user._id, 'watchedAds': ad._id});
+          
+        if (adToAdd == null) {    
+            loggedUser.watchedAds.push(ad);
+            await loggedUser.save();
+            req.flash('success', 'Dodano ogłoszenie do obserwowanych');
+            checkUrlAndRedirect();
+        } else {
+            req.flash('error', 'Te ogłoszenie jest już w twoich obserwowanych');
+            checkUrlAndRedirect();
+        }
     }
 
     function checkUrlAndRedirect() {
+        if (currentPage.url.includes('category')) {
+            return res.redirect(`${currentPage.url}`);
+        }
         if (currentPage.url == `/ads/${ad._id}`) {
             return res.redirect(`/ads/${ad._id}`);
-        } else {
+        }
+        else {
             if (currentPage.searchQuery) {
                 return res.redirect(`/ads/?page=${currentPage.page}&search=${currentPage.searchQuery}#ads`);
             }
@@ -58,13 +62,11 @@ router.get('/:id/remove', async(req, res) => {
     }
 
     const ad = await Ad.findOne({_id: req.params.id});
-    const loggedUser = await User.findOne({_id: req.user._id});
-    const adToRemove = await User.findOne({'watchedAds': mongoose.Types.ObjectId(ad._id)});
+    const adToRemove = await User.findOne({_id: req.user._id, 'watchedAds': mongoose.Types.ObjectId(ad._id)});
     adToRemove.watchedAds.pull({_id: mongoose.Types.ObjectId(ad._id)});
     await adToRemove.save();
     req.flash('success', 'Usunięto ogłoszenie z obserwowanych');
     res.redirect('/watched');
 });
-
 
 module.exports = router;
