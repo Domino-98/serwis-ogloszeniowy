e9625867572f03d426babb176a8eb45a972aa110
Dominik Buczek <bdominoo@gmail.com>
2021-05-31 11:18:57 +0200

    Fix title text overflow, add ad form char limits

--- schemas.js
+++ schemas.js
@@ -1,11 +1,11 @@
 const Joi = require('joi');
 module.exports.adSchema = Joi.object({
     ad: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().max(60).required(),
         category: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        description: Joi.string().required(),
-        contactNumber: Joi.string().trim().regex(/^[0-9]+$/).min(6).max(10),
-        location: Joi.string().required()
+        price: Joi.string().min(0).max(12).pattern(/^[0-9]+$/).required(),
+        description: Joi.string().max(1000).required(),
+        contactNumber: Joi.string().min(6).max(10).pattern(/^[0-9]+$/).required(),
+        location: Joi.string().max(40).required()
     }).required()
 });
